9140bec4216c33ec8604816757c7292aac25ad1a
Naiman Kopeyev <nkopeyev@gmail.com>
2020-09-08 16:23:42 +0600

    docker add port 11777

--- _bash.sh
+++ _bash.sh
@@ -1,2 +1,2 @@
 #docker run --rm -v "$(PWD)":/usr/src/multi-process-web-server -w /usr/src/multi-process-web-server -it gcc:4.9 bash
-docker run --rm -v "$(PWD)":/usr/src/multi-process-web-server -w /usr/src/multi-process-web-server -p 12345:12345 -it rikorose/gcc-cmake bash
+docker run --rm -v "$(PWD)":/usr/src/multi-process-web-server -w /usr/src/multi-process-web-server -p 12345:12345 -p 11777:11777 -it rikorose/gcc-cmake bash
